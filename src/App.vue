a73191c1647151f1b471b22cd9b8df748a2f452b
mrgao125 <312595543@qq.com>
2022-05-10 15:55:03 +0800

    app文件创建

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <!-- 一级路由 显示位置-->
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name:'App'
+  }
+</script>
+
+<style>
+</style>
